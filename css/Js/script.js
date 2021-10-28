7e5e74a36a5c964dc1d1b12313c83d92555bf35d
Salvatore949 <salvo.mess49@gmail.com>
2021-10-28 09:39:19 +0200

    struttura

---
+++ css/Js/script.js
@@ -0,0 +1,24 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+]
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Ungheria',
+    'Maldive'
+]
+
+const text = [
+     'lorem',
+     'lorem ipsum',
+     'Lorem ipsum dolor sit',
+     'lorem ipsum dolor si amet',
+     'lorem ispum dolor sit amet adipiscing elit'
+]  
+
